30ee13a9fbd53804a764360559bcba17b4f2e5ae
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-18 13:50:22 -0400

    Create and setup setting view storyboard; hookup actions and outlets

---
+++ MatchedUp/Classes/ViewControllers/SettingsViewController.m
@@ -0,0 +1,58 @@
+//
+//  SettingsViewController.m
+//  MatchedUp
+//
+//  Created by Joe Gesualdo on 9/18/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import "SettingsViewController.h"
+
+@interface SettingsViewController ()
+
+#pragma mark - IBOutlets
+
+@property (strong, nonatomic) IBOutlet UISlider *ageSlider;
+@property (weak, nonatomic) IBOutlet UISwitch *mensSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *womensSwitch;
+@property (weak, nonatomic) IBOutlet UISwitch *relationshipStatusSwitch;
+@property (weak, nonatomic) IBOutlet UIButton *logoutButton;
+@property (weak, nonatomic) IBOutlet UIButton *editProfileButton;
+
+#pragma mark - IBActions
+
+- (IBAction)logoutButtonPressed:(UIButton *)sender;
+- (IBAction)editProfileButtonPressed:(UIButton *)sender;
+@end
+
+@implementation SettingsViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)logoutButtonPressed:(UIButton *)sender {
+}
+
+- (IBAction)editProfileButtonPressed:(UIButton *)sender {
+}
+@end
