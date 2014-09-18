7134ac06a48fc9bee7a205ecaaaad44d6d4934e3
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-18 15:31:05 -0400

    Add liking and disliking functionality

--- MatchedUp/Classes/ViewControllers/HomeViewController.m
+++ MatchedUp/Classes/ViewControllers/HomeViewController.m
@@ -33,8 +33,13 @@
 // We are going to store all the photos from other users in this photos property
 @property(strong,nonatomic)NSArray *photos;
 @property(strong,nonatomic)PFObject *photo;
+// activies let us keep track of who we liked
+@property(strong, nonatomic)NSMutableArray *activities;
 // This will store the current photo your looking at
 @property(nonatomic)int currentPhotoIndex;
+// for the current use on screen, have i already liked or disliked them
+@property(nonatomic)BOOL isLikedByCurrentUser;
+@property(nonatomic)BOOL isDislikedByCurrentUser;
 
 @end
 
@@ -47,8 +52,8 @@
     // Do any additional setup after loading the view.
     
     // Don't want user to immediately press the like, dislike, or info untile we get all the info from the network
-    self.likeButton.enabled = NO;
-    self.dislikeButton.enabled = NO;
+//    self.likeButton.enabled = NO;
+//    self.dislikeButton.enabled = NO;
     self.infoButton.enabled = NO;
     
     self.currentPhotoIndex = 0;
@@ -94,9 +99,11 @@
 - (IBAction)settingsBarButtonPressed:(UIBarButtonItem *)sender {
 }
 - (IBAction)likeButtonPressed:(UIButton *)sender {
+    [self checkLike];
 }
 
 - (IBAction)dislikeButtonPressed:(UIButton *)sender {
+    [self checkDislike];
 }
 
 - (IBAction)infoButtonPressed:(UIButton *)sender {
@@ -133,4 +140,85 @@
     self.tagLineLabel.text = self.photo[@"user"][@"tagLine"];
 }
 
+-(void)setupNextPhoto
+{
+    // checks if there is another photo
+    if ((self.currentPhotoIndex + 1) < self.photos.count) {
+        self.currentPhotoIndex ++;
+        [self queryForCurrentPhotoIndex];
+    } else {
+        UIAlertView *alertView = [[UIAlertView alloc]initWithTitle:@"No More uesrs to view" message:@"Check back later for more people" delegate:self cancelButtonTitle:@"Ok" otherButtonTitles:nil, nil];
+        [alertView show];
+    }
+}
+
+-(void)saveLike
+{
+    // This is how we create a new class - Activity
+    PFObject *likeActivity = [PFObject objectWithClassName:@"Activity"];
+    [likeActivity setObject:@"like" forKey:@"type"];
+    [likeActivity setObject:[PFUser currentUser] forKey:@"fromUser"];
+    [likeActivity setObject:[self.photo objectForKey:@"user"] forKey:@"toUser"];
+    [likeActivity setObject:self.photo forKey:@"Photo"];
+    [likeActivity saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        self.isLikedByCurrentUser = YES;
+        self.isDislikedByCurrentUser = NO;
+        [self.activities addObject:likeActivity];
+        [self setupNextPhoto];
+    }];
+}
+
+-(void)saveDislike
+{
+    // This is how we create a new class - Activity
+    PFObject *dislikeActivity = [PFObject objectWithClassName:@"Activity"];
+    [dislikeActivity setObject:@"dislike" forKey:@"type"];
+    [dislikeActivity setObject:[PFUser currentUser] forKey:@"fromUser"];
+    [dislikeActivity setObject:[self.photo objectForKey:@"user"] forKey:@"toUser"];
+    [dislikeActivity setObject:self.photo forKey:@"Photo"];
+    [dislikeActivity saveInBackgroundWithBlock:^(BOOL succeeded, NSError *error) {
+        self.isLikedByCurrentUser = NO;
+        self.isDislikedByCurrentUser = YES;
+        [self.activities addObject:dislikeActivity];
+        [self setupNextPhoto];
+    }];
+}
+
+-(void)checkLike
+{
+    // checks if you already like current user
+    if (self.isLikedByCurrentUser) {
+        [self setupNextPhoto];
+        return;
+    // checks if you disliked the user in the past
+    } else if (self.isDislikedByCurrentUser){
+        for (PFObject *activity in self.activities){
+            [activity deleteInBackground];
+        }
+        [self.activities removeLastObject];
+        [self saveLike];
+    // If you havent either liked or disliked the user
+    } else {
+        [self saveLike];
+    }
+}
+
+-(void)checkDislike
+{
+    // checks if you already like current user
+    if (self.isDislikedByCurrentUser) {
+        [self setupNextPhoto];
+        return;
+    // checks if you disliked the user in the past
+    } else if (self.isLikedByCurrentUser){
+        for (PFObject *activity in self.activities){
+            [activity deleteInBackground];
+        }
+        [self.activities removeLastObject];
+        [self saveDislike];
+    // If you havent either liked or disliked the user
+    } else {
+        [self saveDislike];
+    }
+}
 @end
