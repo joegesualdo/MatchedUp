ce2b9c739154ef1b1ce674ec6ecca3efa29bc266
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-18 11:29:54 -0400

    Update user information with the information from facebook when user logs in

--- MatchedUp/Classes/ViewControllers/Login/LoginViewController.m
+++ MatchedUp/Classes/ViewControllers/Login/LoginViewController.m
@@ -69,7 +69,49 @@
                 
             }
         } else {
+            [self updateUserInformation];
             [self performSegueWithIdentifier:@"loginToTabBarSegue" sender:self];
+        }
+    }];
+}
+
+#pragma mark - Helper Methods
+
+// This will update the users information with the info from facebook
+-(void)updateUserInformation
+{
+    // set the request you want to send to facebook
+    // requestForMe - Creates a request representing a Graph API call to the "me" endpoint, using the active session.
+    FBRequest *request = [FBRequest requestForMe];
+    // make that request you defined above
+    [request startWithCompletionHandler:^(FBRequestConnection *connection, id result, NSError *error) {
+        NSLog(@"%@", result);
+        if (!error) {
+            NSDictionary *userDictionary = (NSDictionary *)result;
+            NSMutableDictionary *userProfile = [[NSMutableDictionary alloc]initWithCapacity:8];
+            if (userDictionary[@"name"]) {
+                userProfile[@"name"] = userDictionary[@"name"];
+            }
+            if (userDictionary[@"first_name"]) {
+                userProfile[@"first_name"] = userDictionary[@"first_name"];
+            }
+            if (userDictionary[@"location"][@"name"]) {
+                userProfile[@"location"] = userDictionary[@"location"][@"name"];
+            }
+            if (userDictionary[@"gender"]) {
+                userProfile[@"gender"] = userDictionary[@"gender"];
+            }
+            if (userDictionary[@"birthday"]) {
+                userProfile[@"birthday"] = userDictionary[@"birthday"];
+            }
+            if (userDictionary[@"interested_in"]) {
+                userProfile[@"interested_in"] = userDictionary[@"interested_in"];
+            }
+            
+            [[PFUser currentUser] setObject:userProfile forKey:@"profile"];
+            [[PFUser currentUser] saveInBackground];
+        } else {
+            NSLog(@"Error in FB: %@", error);
         }
     }];
 }
