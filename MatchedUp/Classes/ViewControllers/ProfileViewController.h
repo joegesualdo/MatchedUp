0eeb52d36da1edf055affcafc784e41c53e70397
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-18 13:14:54 -0400

    Get our profile image from parse and display it on our profile page

---
+++ MatchedUp/Classes/ViewControllers/ProfileViewController.h
@@ -0,0 +1,15 @@
+//
+//  ProfileViewController.h
+//  MatchedUp
+//
+//  Created by Joe Gesualdo on 9/18/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@property (weak, nonatomic) IBOutlet UIImageView *profilePictureImageView;
+
+@end
---
+++ MatchedUp/Classes/ViewControllers/ProfileViewController.m
@@ -0,0 +1,60 @@
+//
+//  ProfileViewController.m
+//  MatchedUp
+//
+//  Created by Joe Gesualdo on 9/18/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import "ProfileViewController.h"
+
+@interface ProfileViewController ()
+
+@end
+
+@implementation ProfileViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    
+    // Want all the photos
+    PFQuery *query = [PFQuery queryWithClassName:kPhotoClassKey];
+    // Actually only want current user photos
+    [query whereKey:kPhotoUserKey equalTo:[PFUser currentUser]];
+    
+    // Get our query
+    [query findObjectsInBackgroundWithBlock:^(NSArray *objects, NSError *error) {
+        if ([objects count] > 0){
+            // Get backfirst photo, if there are more
+            PFObject *photo = objects[0];
+            // Get back the actuall image as a PFFile
+            // this only gives us a reference to the file, not the actauly file (image) so we still need to get it from parse
+            PFFile *pictureFile = photo[kPhotoPictureKey];
+            // get the file from parse that wer received a reference to
+            [pictureFile getDataInBackgroundWithBlock:^(NSData *data, NSError *error) {
+                // Once we get the data we set the image in our profile
+                self.profilePictureImageView.image = [UIImage imageWithData:data];
+            }];
+        }
+    }];
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
+{
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
