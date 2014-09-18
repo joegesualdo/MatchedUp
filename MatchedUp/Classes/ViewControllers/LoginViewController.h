c614e68b3523d84b75de6c6673b14a6a42b1cba2
Joe Gesualdo <joegesualdo@gmail.com>
2014-09-18 09:36:37 -0400

    Add navigation controller, login view, and tab bar controller in storyboard

---
+++ MatchedUp/Classes/ViewControllers/LoginViewController.h
@@ -0,0 +1,13 @@
+//
+//  LoginViewController.h
+//  MatchedUp
+//
+//  Created by Joe Gesualdo on 9/18/14.
+//  Copyright (c) 2014 joegesualdo. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface LoginViewController : UIViewController
+
+@end
